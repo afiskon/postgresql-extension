27f16cd0e8ed49be282b4e169d0dde7b14e757fd
Aleksander Alekseev <afiskon@gmail.com>
2023-02-13 15:14:24 +0300

    Fix

--- 007-table-access-scankey/experiment.c
+++ 007-table-access-scankey/experiment.c
@@ -11,17 +11,13 @@
 #include <utils/snapmgr.h>
 
 PG_MODULE_MAGIC;
-PG_FUNCTION_INFO_V1(phonebook_insert);
-PG_FUNCTION_INFO_V1(phonebook_lookup_seqscan);
-PG_FUNCTION_INFO_V1(phonebook_update_simple);
-PG_FUNCTION_INFO_V1(phonebook_delete_simple);
 PG_FUNCTION_INFO_V1(phonebook_lookup_seqscan_deform);
-PG_FUNCTION_INFO_V1(phonebook_lookup_index);
 
 #define PHONEBOOK_TABLE_NAME "phonebook"
 #define PHONEBOOK_PKEY_SEQ_NAME "phonebook_id_seq"
 #define PHONEBOOK_NAME_IDX_NAME "phonebook_name_idx"
 
+/*
 typedef struct FormData_phonebook
 {
 	int32 id;
@@ -30,6 +26,7 @@ typedef struct FormData_phonebook
 } FormData_phonebook;
 
 typedef FormData_phonebook* Form_phonebook;
+*/
 
 typedef enum Anum_phonebook
 {
@@ -56,132 +53,6 @@ name_to_oid(const char* name)
 }
 
 Datum
-phonebook_insert(PG_FUNCTION_ARGS)
-{
-	Relation rel;
-	HeapTuple tup;
-	Datum values[Natts_phonebook];
-	bool nulls[Natts_phonebook];
-	Name name = PG_GETARG_NAME(0);
-	int32 phone = PG_GETARG_INT32(1);
-	Oid tbl_oid = name_to_oid(PHONEBOOK_TABLE_NAME);
-	Oid pkey_seq_oid = name_to_oid(PHONEBOOK_PKEY_SEQ_NAME);
-	int32 next_id = (int32)DatumGetInt64(DirectFunctionCall1(nextval_oid, ObjectIdGetDatum(pkey_seq_oid)));
-
-	memset(nulls, false, sizeof(nulls));
-
-	rel = table_open(tbl_oid, RowExclusiveLock);
-
-	// or: AttrNumberGetAttrOffset(Anum_phonebook_id)
-	values[Anum_phonebook_id - 1] = Int32GetDatum(next_id);
-	values[Anum_phonebook_name - 1] = NameGetDatum(name);
-	values[Anum_phonebook_phone - 1] = Int32GetDatum(phone);
-
-	tup = heap_form_tuple(RelationGetDescr(rel), values, nulls);
-
-	/* inserts a new heap tuple, keeping indexes current */
-	CatalogTupleInsert(rel, tup);
-	heap_freetuple(tup);
-
-	table_close(rel, RowExclusiveLock);
-	PG_RETURN_INT32(next_id);
-}
-
-Datum
-phonebook_lookup_seqscan(PG_FUNCTION_ARGS)
-{
-	Relation rel;
-	HeapTuple tup;
-	TableScanDesc scan;
-	int32 found_phone = -1;
-	Name name = PG_GETARG_NAME(0);
-	Oid tbl_oid = name_to_oid(PHONEBOOK_TABLE_NAME);
-
-	rel = table_open(tbl_oid, AccessShareLock);
-	scan = table_beginscan(rel, GetTransactionSnapshot(), 0, NULL);
-
-	while ((tup = heap_getnext(scan, ForwardScanDirection)) != NULL)
-	{
-		Form_phonebook record = (Form_phonebook) GETSTRUCT(tup);
-
-		if(strcmp(record->name.data, name->data) == 0)
-		{
-			found_phone = record->phone;
-			break;
-		}		
-	}
-
-	table_endscan(scan);
-	table_close(rel, AccessShareLock);
-	PG_RETURN_INT32(found_phone);
-}
-
-Datum
-phonebook_update_simple(PG_FUNCTION_ARGS)
-{
-	Name name = PG_GETARG_NAME(0);
-	int32 newphone = PG_GETARG_INT32(1);
-	Relation rel;
-	HeapTuple tup;
-	TableScanDesc scan;
-	int32 found_phone = -1;
-	Oid tbl_oid = name_to_oid(PHONEBOOK_TABLE_NAME);
-
-	rel = table_open(tbl_oid, RowExclusiveLock);
-	scan = table_beginscan(rel, GetTransactionSnapshot(), 0, NULL);
-
-	while ((tup = heap_getnext(scan, ForwardScanDirection)) != NULL)
-	{
-		Form_phonebook record = (Form_phonebook) GETSTRUCT(tup);
-		if(strcmp(record->name.data, name->data) == 0)
-		{
-			HeapTuple newtup = heap_copytuple(tup);
-			found_phone = record->phone;
-			((Form_phonebook) GETSTRUCT(newtup))->phone = newphone;
-
-			/* updates a heap tuple, keeping indexes current */
-			CatalogTupleUpdate(rel, &newtup->t_self, newtup);
-			heap_freetuple(newtup);
-			break;
-		}		
-	}
-
-	table_endscan(scan);
-	table_close(rel, RowExclusiveLock);
-	PG_RETURN_INT32(found_phone);
-}
-
-Datum
-phonebook_delete_simple(PG_FUNCTION_ARGS)
-{
-	Name name = PG_GETARG_NAME(0);
-	Relation rel;
-	HeapTuple tup;
-	TableScanDesc scan;
-	int32 found_phone = -1;
-	Oid tbl_oid = name_to_oid(PHONEBOOK_TABLE_NAME);
-
-	rel = table_open(tbl_oid, RowExclusiveLock);
-	scan = table_beginscan(rel, GetTransactionSnapshot(), 0, NULL);
-
-	while ((tup = heap_getnext(scan, ForwardScanDirection)) != NULL)
-	{
-		Form_phonebook record = (Form_phonebook) GETSTRUCT(tup);
-		if(strcmp(record->name.data, name->data) == 0)
-		{
-			found_phone = record->phone;
-			/* deletes a heap tuple, keeping indexes current */
-			CatalogTupleDelete(rel, &tup->t_self);
-			break;
-		}		
-	}
-
-	table_endscan(scan);
-	table_close(rel, RowExclusiveLock);
-	PG_RETURN_INT32(found_phone);
-}
-
-Datum
 phonebook_lookup_seqscan_deform(PG_FUNCTION_ARGS)
 {
 	Relation rel;
@@ -205,62 +76,18 @@ phonebook_lookup_seqscan_deform(PG_FUNCTION_ARGS)
 
 		if(strcmp(rec_name->data, name->data) == 0)
 		{
+			if(isnull[Anum_phonebook_phone - 1] == true)
+			{
+				found_phone = -2;
+				break;
+			}
+
 			found_phone = DatumGetInt32(values[Anum_phonebook_phone - 1]);
 			break;
 		}		
 	}
 
 	table_endscan(scan);
-	table_close(rel, AccessShareLock);
-	PG_RETURN_INT32(found_phone);
-}
-
-Datum
-phonebook_lookup_index(PG_FUNCTION_ARGS)
-{
-	Relation rel, idxrel;
-	IndexScanDesc scan;
-	TupleTableSlot* slot;
-	HeapTuple tup;
-	ScanKeyData skey[1];
-	int32 found_phone = -1;
-	Name name = PG_GETARG_NAME(0);
-	Oid tbl_oid = name_to_oid(PHONEBOOK_TABLE_NAME);
-	Oid idx_oid = name_to_oid(PHONEBOOK_NAME_IDX_NAME);
-
-	rel = table_open(tbl_oid, AccessShareLock);
-	idxrel = index_open(idx_oid, AccessShareLock);
-	
-	scan = index_beginscan(rel, idxrel, GetTransactionSnapshot(), 1 /* nkeys */, 0 /* norderbys */);
-
-	ScanKeyInit(&skey[0],
-				Anum_phonebook_name_idx_name, /* numeration starts from 1; idx, not rel! */
-				BTEqualStrategyNumber, F_NAMEEQ,
-				NameGetDatum(name));
-	index_rescan(scan, skey, 1, NULL /* orderbys */, 0 /* norderbys */);
-
-	slot = table_slot_create(rel, NULL);
-	while (index_getnext_slot(scan, ForwardScanDirection, slot))
-	{
-		Form_phonebook record;
-		bool should_free;
-
-		tup = ExecFetchSlotHeapTuple(slot, false, &should_free);
-		record = (Form_phonebook) GETSTRUCT(tup);
-
-		if(strcmp(record->name.data, name->data) == 0)
-		{
-			found_phone = record->phone;
-			if(should_free) heap_freetuple(tup);
-			break;
-		}
-
-		if(should_free) heap_freetuple(tup);
-	}
-
-	index_endscan(scan);
-	ExecDropSingleTupleTableSlot(slot);
-	table_close(idxrel, AccessShareLock);
 	table_close(rel, AccessShareLock);
 	PG_RETURN_INT32(found_phone);
 }
--- 007-table-access-scankey/sql/experiment.sql
+++ 007-table-access-scankey/sql/experiment.sql
@@ -1,28 +1,10 @@
 CREATE EXTENSION experiment;
 
-SELECT phonebook_insert('Alice', 123);
-SELECT * FROM phonebook;
-
-SELECT phonebook_insert('Bob', 456);
-SELECT * FROM phonebook;
-
-SELECT phonebook_lookup_seqscan('Alice');
-SELECT phonebook_lookup_seqscan('Bob');
-SELECT phonebook_lookup_seqscan('Charlie');
+INSERT INTO phonebook (id, name, phone) VALUES
+(1, 'Alice', 123456),
+(2, 'Bob', NULL);
 
 SELECT phonebook_lookup_seqscan_deform('Alice');
 SELECT phonebook_lookup_seqscan_deform('Bob');
-SELECT phonebook_lookup_seqscan_deform('Charlie');
-
-SELECT phonebook_lookup_index('Alice');
-SELECT phonebook_lookup_index('Bob');
-SELECT phonebook_lookup_index('Charlie');
-
-SELECT phonebook_update_simple('Alice', 789);
-SELECT phonebook_lookup_index('Alice');
-
-SELECT phonebook_delete_simple('Alice');
-SELECT phonebook_lookup_index('Alice');
-SELECT phonebook_delete_simple('Charlie');
 
 DROP EXTENSION experiment;
